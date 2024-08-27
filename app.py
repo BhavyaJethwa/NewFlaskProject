69191ef790824954fd790409864acd49e953c359
BhavyaJethwa <bhavyajethwa11@gmail.com>
2024-08-27 11:54:21 +0530

    Final push

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , make_response , jsonify
+from flask import Flask , make_response , jsonify, render_template
 from model import db
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'HELLOOO /n Working'
+    return render_template('home.html')
 @app.route('/api/products' , methods=['GET', 'POST'])
 def products():
     return make_response(jsonify({"products" : db}),200)
